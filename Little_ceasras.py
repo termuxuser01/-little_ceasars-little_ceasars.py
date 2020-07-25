4d3d10713cb06f1a1f5690af577a5aa138cfc304
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2020-07-25 13:22:09 -0400

    changed the definition of map_dict to be cleaner

--- Little_ceasras.py
+++ Little_ceasras.py
@@ -1,9 +1,16 @@
 import string
+
+
 opt = input('enter "e" for encoding or "d" for decoding')
 str_org = input("enter desired string:/n")
 b_len = int(input("enter a text block size /n default value: 5/n"))
-sub_key = int(input("enter a jump key: "))
-map_dict = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15, "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
+sub_key = int(input("enter a shift key: "))
+
+map_dict = dict()
+
+for i, l in enumerate(string.ascii_lowercase):
+  map_dict[i+1] = l
+
 
 def option(ed):
     if ed == "e":
